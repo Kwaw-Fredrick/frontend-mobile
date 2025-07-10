5b81b01c6e4e7895b4efb248781c696ea43592c2
kwaw-fredrick <kwawfredrick35@gmail.com>
2025-07-10 11:34:06 -0900

    login part

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -1,10 +1,86 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Image,
+  Platform,
+  TextInput,
+} from "react-native";
+import styles from "../../styles/login.styles";
+import { useState } from "react";
+import {Ionicons} from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsloading] = useState(false)
   return (
-    <View>
-      <Text>Login screen</Text>
-    </View>
-  )
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        {/*top Illustration*/}
+
+        <View style={styles.topIllustration}>
+          <Image
+            source={require("../../assets/images/login-pana.png")}
+            style={styles.illustrationImage}
+            resizeMode="contain"
+          />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.formContainer}>
+              {/*email*/}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Email</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons 
+                name="mail-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+                />
+
+                <TextInput
+                style={styles.input}
+                placeholder="Enter your email"
+                placeholderTextColor={COLORS.placeholderText}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                />
+              </View>
+            </View>
+
+            {/*password*/}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Password</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                name="lock-closed-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+                />
+
+                <TextInput
+                style={styles.input}
+                placeholder="Enter your password"
+                placeholderTextColor={COLORS.placeholderText}
+                value={password}
+                onChange={setPassword}
+                secureTextEntry= {!showPassword}
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
--- components/safeScreen.jsx
+++ components/safeScreen.jsx
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import COLORS from "../constants/colors";
 
-export default function SafeScreen(children) {
+export default function SafeScreen({children}) {
   const insets = useSafeAreaInsets();
   return (
     <View style={[styles.container, { paddingTop: insets.top }]}>
